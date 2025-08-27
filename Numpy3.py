d1e6e8b4a6fb930b5d958018f8b137dfa69073cc
Kartik Attri <attrikartik494@gmail.com>
2025-08-27 10:49:01 +0530

    module 4 started

---
+++ Numpy3.py
@@ -0,0 +1,13 @@
+#np.array([True, 1, 2]) + np.array([3, 4, False])
+#The numpy package is already imported as np.
+
+#Possible answers
+
+
+#1 np.array([True, 1, 2, 3, 4, False])
+
+#2 np.array([4, 3, 0]) + np.array([0, 2, 2])                  CORRECT
+
+#3 np.array([1, 1, 2]) + np.array([3, 4, -1])
+
+#4 np.array([0, 1, 2, 3, 4, 5])
--- packages2.py
+++ packages2.py
@@ -9,3 +9,25 @@ A = pi * 0.43 ** 2
 
 print("Circumference: " + str(C))
 print("Area: " + str(A))
+
+
+
+
+
+#Suppose you want to use the function inv(), which is in the linalg subpackage of the scipy package. You want to be able to use this function as follows:
+
+#my_inv([[1,2], [3,4]])
+#Which import statement will you need in order to run the above code without an error?
+
+#Possible answers
+
+
+#1 import scipy
+
+#2 import scipy.linalg
+
+#3 from scipy.linalg import my_inv
+
+#4 from scipy.linalg import inv as my_inv
+
+# ANS--------------- OPTION 4 IS CORRECT
