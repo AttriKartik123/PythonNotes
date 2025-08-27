c068eb7eb5a7b079de1b943bc677067fdaec301e
Kartik Attri <attrikartik494@gmail.com>
2025-08-27 10:01:45 +0530

    packages

---
+++ packages.py
@@ -0,0 +1,15 @@
+#packages 
+# Import the math package
+import math 
+
+# Calculate C   #0.43 is r and 2 is given 
+C = 2 * 0.43 * math.pi
+
+# Calculate A
+A = math.pi * 0.43 ** 2
+
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+
+
+#--------------------------------------------------------------------------------------------
